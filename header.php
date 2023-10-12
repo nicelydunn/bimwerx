939d6b4148e3ee98090333c4a6208336473e851c
nicelydunn <joey.dunn@gmail.com>
2023-10-12 15:06:00 -0400

    Updated Logo

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'bimwerx' ); ?></a>
 	<header class="bg-white flex justify-between px-4 py-2 gap-8 items-center top-0 fixed z-[99] w-full border-b border-gray-100">
-		<div class="lg:order-1"><a href="/" aria-label="BIMWERX Homepage"><img class="h-14 w-auto" src="<?php echo get_template_directory_uri(); ?>/assets/images/bimwerx-logo.png" alt="BIMWERX Logo"></a></div>
+		<div class="lg:order-1"><a href="/" aria-label="BIMWERX Homepage"><img class="h-14 w-auto" src="<?php echo get_template_directory_uri(); ?>/assets/images/bimwerx-logo-rebrand.png" alt="BIMWERX Logo"></a></div>
 		<div class="gap-2 items-center hidden lg:flex lg:order-3">
 			<div><a href="tel:888-333-8804">(888) 333-8804</a></div>
 			<div><a href="/contact-us/" class="btn btn-primary"><?php esc_html_e('Let\'s Talk', 'bimwerx'); ?></a></div>
