70ded9dedd4ad7076dac19f83583d53c82c423c4
nicelydunn <joey.dunn@gmail.com>
2023-10-23 11:24:28 -0400

    Updated 404 page

--- 404.php
+++ 404.php
@@ -10,51 +10,24 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'bimwerx' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'bimwerx' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'bimwerx' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$bimwerx_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'bimwerx' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$bimwerx_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+	<main id="primary" class="site-main mt-28">
+		<div class="container mx-auto px-4">
+			<h1 class="text-4xl">So Sorry!</h1>
+			<div class="w-20 border-b-4 border-orange-dark mt-4"></div>
+			<div class="prose-lg">
+				<h2>The page you are looking for cannot be found</h2>
+				<p>Error code: 404</p>
+				<p>Here are some helpful links instead</p>
+				<ul>
+					<li><a href="/">Home</a></li>
+					<li><a href="/services/">Services</a></li>
+					<li><a href="/case-study/">Case Studies</a></li>
+					<li><a href="/news/">News</a></li>
+					<li><a href="/about-bimwerx/">About Us</a></li>
+				</ul>
+			</div>
+		</div>
+	</main>
 
 <?php
 get_footer();
